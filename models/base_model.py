5afe08e05590b4f55a06d081d148a70efe9a8654
PabloOsorix <3387@holbertonschool.com>
2022-03-01 23:54:28 -0500

    :alien: update files

--- models/base_model.py
+++ models/base_model.py
@@ -15,20 +15,17 @@ class BaseModel(ABC):
         kwargs = kwargs
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
+        self.update_at = datetime.now()
 
     def __str__(self):
-        string = ""
-        string = "[{}] ({}) {}".format(type(self).__name__, self.id, self.created_at)
-        return string
+        return "[{}] ({}) {}".format(type(self).__name__, self.id,
+                                        self.__dict__)
         
+    def save(self):
+        self.update_at = datetime.now()
         
-        
-    
+    def to_dict(self):
+        return "{}".format(self.__dict__)
 
 base = BaseModel()
-print(base.id)
-base2 = BaseModel()
-print(base2.id)
-print(base.created_at)
-
-print(base)
+print(base.to_dict)

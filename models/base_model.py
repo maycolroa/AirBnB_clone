7cec56900bb49918bdaf9d15172c67c6e9f67bea
PabloOsorix <3387@holbertonschool.com>
2022-03-02 23:39:42 -0500

    :pencil2: add storage variable

--- models/base_model.py
+++ models/base_model.py
@@ -24,7 +24,8 @@ class BaseModel(ABC):
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.update_at = datetime.now()
-            storage.new(self)
+
+        storage.new(self)
 
     def __str__(self):
         """instance that return str of class atributtes"""

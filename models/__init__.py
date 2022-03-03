828147c068c891a3b72df077621b67717d26de15
PabloOsorix <3387@holbertonschool.com>
2022-03-02 22:45:22 -0500

    :heavy_plus_sign: create storage and storage.reload variable

---
+++ models/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+module that create a unique FileStorage
+instance for your application.
+"""
+
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload

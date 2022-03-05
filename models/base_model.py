2a11be5216132de1fb3eae201034b9eed59c5f4f
PabloOsorix <3387@holbertonschool.com>
2022-03-05 16:03:39 -0500

    :heavy_plus_sign: add
    a

--- models/base_model.py
+++ models/base_model.py
@@ -26,8 +26,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
-            storage.save()
+
 
     def __str__(self):
         """

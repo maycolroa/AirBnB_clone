cc03f6986f0fea18e98fbe88161b699016ccddb1
PabloOsorix <3387@holbertonschool.com>
2022-03-05 15:33:50 -0500

    :recycle: refact my code

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,49 +1,44 @@
 #!/usr/bin/python3
-"""
-Class that defines FileStorage
-"""
+"""Module that contains class FilesStorage"""
 import json
 import os
 
 
 class FileStorage():
-    """
-        Initialize private FileStorage class attributes
+    """class that realice serialization
+    and deserealization] JSON files.
     """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """
-            Returns the __objects dictionary
-        """
-        return(self.__objects)
+
+        """return all the objects of dict"""
+        return (self.__objects)
 
     def new(self, obj):
-        """
-            Creates a new key(class.id) & value(instance attributes dictionary)
-            of an instance in __objects dictionary
+        """sets in __objects the obj
+        with key <obj class name>.id
         """
         if obj is not None:
             self.__objects.update(
                 {str(type(obj).__name__ + "." + obj.id): obj})
 
     def save(self):
+        """serializes __objects to the
+        JSON file (path: __file_path)
         """
-            Append all keys & values set on __objects dictionary
-            into a new dictionary to save all instances in a json file
-        """
-        dict_serialized = {}
+        new_dict = {}
         if self.__objects is not None:
             for key, value in self.__objects.items():
-                dict_serialized[key] = value.to_dict()
-        with open(self.__file_path, mode="w", encoding="utf-8") as my_file:
-            json.dump(dict_serialized, my_file)
+                new_dict[key] = value.to_dict()
+        with open(self.__file_path, mode="w", encoding="utf-8") as file:
+            json.dump(new_dict, file)
 
     def reload(self):
-        """
-            Load the .json file(verify existence), set all keys & values into
-            the __objects dictionary and recreate instances found in the file
+        """deserializes the JSON file to __objects
+        (only if the JSON file (__file_path) exists;
+        otherwise, do nothing.
         """
         from models.base_model import BaseModel
         from models.user import User
@@ -53,18 +48,18 @@ class FileStorage():
         from models.place import Place
         from models.review import Review
 
-        dict_deserialized = {}
+        deserialized = {}
         if os.path.exists(self.__file_path):
-            with open(self.__file_path, encoding="utf-8") as my_file:
-                content = my_file.read()
+            with open(self.__file_path, encoding="utf-8") as file:
+                contents = file.read()
+
         else:
             return
-        if content is not None or bool(content) is True:
-            dict_deserialized = json.loads(content)
-        for key, value in dict_deserialized.items():
-            if key not in self.__objects.keys():
-                ClassName = value["__class__"]
-                new_instance = eval("{}(**value)".format(ClassName))
-                self.new(new_instance)
+        if contents is not None:
+            deserialized = json.loads(contents)
+            for key, value in deserialized.items():
+                if key not in self.__objects.keys():
+                    new_obj = eval(value["__class__"])(**value)
+                    self.new(new_obj)
         else:
             pass

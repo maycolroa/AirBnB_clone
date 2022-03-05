9ee43d31e12f0fd1ad51d31b7e1f89056f300cd4
PabloOsorix <3387@holbertonschool.com>
2022-03-05 12:26:52 -0500

    :heavy_plus_sign: add line 27

--- models/base_model.py
+++ models/base_model.py
@@ -24,8 +24,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.update_at = datetime.now()
-
-        storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         """instance that return str of class atributtes"""

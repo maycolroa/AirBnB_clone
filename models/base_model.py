da32e3041189e8b4f9992d3f111ddb89f6b49c83
maycolroa <lehallo@hotmail.com>
2022-03-06 20:22:15 -0500

    0x00. AirBnB clone - The console  0.12  06/03/2022

--- models/base_model.py
+++ models/base_model.py
@@ -18,8 +18,7 @@ class BaseModel():
         if kwargs is not None and len(kwargs) > 0:
             for key, value in kwargs.items():
                 if key == "updated_at" or key == "created_at":
-                    setattr(self, key, datetime.strptime(value,
-                            "%Y-%m-%dT%H:%M:%S.%f"))
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
         else:
@@ -32,8 +31,7 @@ class BaseModel():
         """
             Modify the stdr output with a specific format
         """
-        return "[{}] ({}) {}".format(type(self).__name__, self.id,
-                                    self.__dict__)
+        return "[{}] ({}) {}".format(type(self).name, self.id, self.__dict__)
 
     def save(self):
         """
@@ -47,8 +45,8 @@ class BaseModel():
         """
             Return a Dictionary with specific attributes and format
         """
-        new_dict = self.__dict__.copy()
-        new_dict.update({'created_at': self.created_at.isoformat(),
-                        'updated_at': self.updated_at.isoformat(),
-                        '__class__': type(self).__name__})
-        return new_dict
+        representation = self.__dict__.copy()
+        representation["updated_at"] = self.updated_at.isoformat()
+        representation["created_at"] = self.created_at.isoformat()
+        representation["__class__"] = self.__class__.__name__
+        return representation

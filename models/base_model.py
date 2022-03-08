94e6c18db1300cb3d15fabd6be222ab8e52a2f71
maycolroa <lehallo@hotmail.com>
2022-03-08 00:10:47 -0500

    0x00. AirBnB clone - The console  0.18  07/03/2022

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,6 @@
 """
 import uuid
 from datetime import datetime
-from models import storage
 import models
 
 
@@ -28,7 +27,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """
